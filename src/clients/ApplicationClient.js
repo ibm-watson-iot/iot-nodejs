c0da9bb7c6eeddace54fceb9b2512c950e15e02e
Tom Klapiscak <klapitom@uk.ibm.com>
2019-03-22 17:22:12 +0000

    [MINOR] Add methods for interacting with HTTP rule resources

--- src/clients/ApplicationClient.js
+++ src/clients/ApplicationClient.js
@@ -1788,4 +1788,73 @@ export default class ApplicationClient extends BaseClient {
       })
     })
   }
+
+
+
+
+
+  
+
+  
+  getRulesForLogicalInterface(logicalInterfaceId) {
+    if (this.draftMode) { 
+      return this.getRulesForLogicalInterface(logicalInterfaceId);
+    } else {
+      return this.getActiveRulesForLogicalInterface(logicalInterfaceId);
+    }
+
+  }
+
+  getDraftRulesForLogicalInterface(logicalInterfaceId) {
+    return this.callApi('GET', 200, true, ['draft', 'logicalinterfaces', logicalInterfaceId, 'rules']);
+  }
+
+  getActiveRulesForLogicalInterface(logicalInterfaceId) {
+    return this.callApi('GET', 200, true, ['logicalinterfaces', logicalInterfaceId, 'rules']);
+  }
+
+
+  
+
+
+  createRule(logicalInterfaceId, name, condition, description=undefined, notificationStrategy=ApplicationClient.RuleNotificationStrategy.EVERY_TIME()) {
+    var body = {
+      name,
+      condition,
+      notificationStrategy
+    }
+    if (description) body['description'] = description;
+    var base = this.draftMode ? ['draft', 'logicalinterfaces', logicalInterfaceId, 'rules'] : ['logicalinterfaces', logicalInterfaceId, 'rules'];
+    return this.callApi('POST', 201, true, base, JSON.stringify(body));
+  }
+
+  updateRule(rule) {
+    var base = this.draftMode ? ['draft', 'logicalinterfaces', rule.logicalInterfaceId, 'rules', rule.id] : ['logicalinterfaces', rule.logicalInterfaceId, 'rules', rule.id];
+    return this.callApi('PUT', 200, true, base, JSON.stringify(rule));
+  }
+
+  deleteRule(logicalInterfaceId, ruleId) {
+    var base = this.draftMode ? ['draft', 'logicalinterfaces', logicalInterfaceId, 'rules', ruleId] : ['logicalinterfaces', logicalInterfaceId, 'rules', ruleId];
+    return this.callApi('DELETE', 204, false, base);
+  }
+
 }
+
+
+ApplicationClient.RuleNotificationStrategy = {
+  EVERY_TIME: () => ({
+      when: 'every-time'
+  }),
+  BECOMES_TRUE: () => ({
+    when: 'becomes-true',
+  }),
+  X_IN_Y: (count) => ({
+    when: 'x-in-y',
+    count,
+  }),
+  PERSISTS: (count, timePeriod) => ({
+    when: 'persists',
+    count,
+    timePeriod
+  }),
+};
